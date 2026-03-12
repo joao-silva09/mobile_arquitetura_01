01e540f2fe00cce824660f31f3f6ffb9fabbe135
joao-silva09 <joao.cliick@gmail.com>
2026-03-11 21:41:49 -0300

    style: format getProducts method for improved readability

--- lib/data/datasources/product_remote_datasource.dart
+++ lib/data/datasources/product_remote_datasource.dart
@@ -7,7 +7,9 @@ class ProductRemoteDatasource {
   final http.Client client;
   ProductRemoteDatasource(this.client);
   Future<List<ProductModel>> getProducts() async {
-    final response = await client.get(Uri.parse("https://fakestoreapi.com/products"));
+    final response = await client.get(
+      Uri.parse("https://fakestoreapi.com/products"),
+    );
     final List data = jsonDecode(response.body);
     return data.map((json) => ProductModel.fromJson(json)).toList();
   }
